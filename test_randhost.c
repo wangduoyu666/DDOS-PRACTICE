741f7c44d7d7faac3ab89a83b5971095b92dbce0
tik <78137991+wangduoyu666@users.noreply.github.com>
2022-02-18 17:14:19 +0800

    Update test_randhost.c

--- test_randhost.c
+++ test_randhost.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 int main(void)
 {
-int i,n;
-n=0xFFFFFFFF;
-for(i=0; i<0xFFFFFFFF;i++){
-printf("%d.%d.%d.%d\n", rand()%0xFF,rand()%0xFF,rand()%0xFF,rand()%0xFF);
+   int i;
+   int host[i];
+   for( host[i] = 0 ; host[i] < 0xFFFFFFFF ; host[i]++ ) {
+           host[1]=rand()%0xFF;
+           host[2]=rand()%0xFF;
+           host[3]=rand()%0xFF;
+           host[4]=rand()%0xFF;
+      printf("%d.%d.%d.%d\n", host[1],host[2],host[3],host[4]);
+   }
+   return(0);
 }
-return(0);
-}
+
+
 //just a test or a draft
