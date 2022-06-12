01c634c24cacb6390b39246f8f0d204fbbcab3c3
tik <78137991+wangduoyu666@users.noreply.github.com>
2022-06-12 23:07:48 +0800

    Create traversal.py

---
+++ python/traversal.py
@@ -0,0 +1,15 @@
+import os
+import os.path
+
+"""
+fullpath = os.listdir()
+fp = input("enter your fullpath:")
+pathname = os.open(fp)
+if os.path.isdir(pathname):
+    for pathname in fp:
+        pn = os.scandir(pathname)
+        print(os.listdir(pn))
+else:
+    print(os.path.basename(pathname))
+ def tra()
+ """
